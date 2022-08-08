a3a1e2b027e3c41157c26b18fbcc564c0b1fffc6
abhishek0chauhan <abhi45chauhan@gmail.com>
2022-08-09 01:16:16 +0530

    video quality fix and audio echo fix

--- main.js
+++ main.js
@@ -27,6 +27,14 @@ const servers = {
   ],
 };
 
+let constraints = {
+  video: {
+    width: { min: 640, ideal: 1920, max: 1920 },
+    height: { min: 480, ideal: 1080, max: 1080 },
+  },
+  audio: true,
+};
+
 let init = async () => {
   client = await AgoraRTM.createInstance(APP_ID);
 
@@ -42,10 +50,7 @@ let init = async () => {
 
   client.on("MessageFromPeer", handleMessageFromPeer);
 
-  localStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
+  localStream = await navigator.mediaDevices.getUserMedia(constraints);
   document.getElementById("user-1").srcObject = localStream;
 };
 
